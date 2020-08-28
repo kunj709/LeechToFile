db1278142854a9520afbf92a5bc806472344b863
kunj709 <70050000+kunj709@users.noreply.github.com>
2020-08-28 09:18:23 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -75,7 +75,7 @@ if __name__ == "__main__" :
     #
 #    incoming_gdrive_message_handler = MessageHandler(
 #        incoming_gdrive_message_f,
-#        filters=Filters.command(["gleech@assfile_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
+#        filters=Filters.command([f"{GLEECH_COMMAND}"])  & Filters.chat(chats=AUTH_CHANNEL)
 #    )
 #    app.add_handler(incoming_gdrive_message_handler)
     #
